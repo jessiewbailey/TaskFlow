e6c1fcdcd34b7800b58000d22d0b5b609c88201a
jessiewbailey <jessiewbailey@gmail.com>
2025-07-22 10:33:31 -0400

    relabeled "request text" to "task text"

--- frontend/src/components/WorkflowBlockEditor.tsx
+++ frontend/src/components/WorkflowBlockEditor.tsx
@@ -221,7 +221,7 @@ export const WorkflowBlockEditor: React.FC<WorkflowBlockEditorProps> = ({
                       })}
                       className="border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                     >
-                      <option value="REQUEST_TEXT">Request Text</option>
+                      <option value="REQUEST_TEXT">Task Text</option>
                       <option value="BLOCK_OUTPUT">Block Output</option>
                     </select>
                     
